1e7741606f11b1d04d4e8469a5d5fde108b2badf
jackychan@virtuosity-global.com <jackychan@virtuosity-global.com>
2020-11-24 14:14:53 +0800

    add navigator per destination view

--- lib/main.dart
+++ lib/main.dart
@@ -27,6 +27,114 @@ class DestinationView extends StatefulWidget {
 }
 
 class _DestinationViewState extends State<DestinationView> {
+  @override
+  Widget build(BuildContext context) {
+    return Navigator(
+      onGenerateRoute: (RouteSettings settings) {
+        return MaterialPageRoute(
+          settings: settings,
+          builder: (BuildContext context) {
+            switch (settings.name) {
+              case '/':
+                return RootPage(destination: widget.destination);
+              case '/list':
+                return ListPage(destination: widget.destination);
+              case '/text':
+                return TextPage(destination: widget.destination);
+            }
+          },
+        );
+      },
+    );
+  }
+}
+
+class RootPage extends StatelessWidget {
+  const RootPage({Key key, this.destination}) : super(key: key);
+
+  final Destination destination;
+
+  @override
+  Widget build(BuildContext context) {
+    return Scaffold(
+      appBar: AppBar(
+        title: Text(destination.title),
+        backgroundColor: destination.color,
+      ),
+      backgroundColor: destination.color[50],
+      body: SizedBox.expand(
+        child: InkWell(
+          onTap: () {
+            Navigator.pushNamed(context, "/list");
+          },
+        ),
+      ),
+    );
+  }
+}
+
+class ListPage extends StatelessWidget {
+  const ListPage({Key key, this.destination}) : super(key: key);
+
+  final Destination destination;
+
+  @override
+  Widget build(BuildContext context) {
+    const List<int> shades = <int>[
+      50,
+      100,
+      200,
+      300,
+      400,
+      500,
+      600,
+      700,
+      800,
+      900
+    ];
+
+    return Scaffold(
+      appBar: AppBar(
+        title: Text(destination.title),
+        backgroundColor: destination.color,
+      ),
+      backgroundColor: destination.color[50],
+      body: SizedBox.expand(
+        child: ListView.builder(
+          itemCount: shades.length,
+          itemBuilder: (BuildContext context, int index) {
+            return SizedBox(
+              height: 128,
+              child: Card(
+                color: destination.color[shades[index]].withOpacity(0.25),
+                child: InkWell(
+                  onTap: () {
+                    Navigator.pushNamed(context, "/text");
+                  },
+                  child: Center(
+                    child: Text('Item $index',
+                        style: Theme.of(context).primaryTextTheme.headline4),
+                  ),
+                ),
+              ),
+            );
+          },
+        ),
+      ),
+    );
+  }
+}
+
+class TextPage extends StatefulWidget {
+  const TextPage({Key key, this.destination}) : super(key: key);
+
+  final Destination destination;
+
+  @override
+  _TextPageState createState() => _TextPageState();
+}
+
+class _TextPageState extends State<TextPage> {
   TextEditingController _textController;
 
   @override
@@ -41,10 +149,10 @@ class _DestinationViewState extends State<DestinationView> {
   Widget build(BuildContext context) {
     return Scaffold(
       appBar: AppBar(
-        title: Text('${widget.destination.title} Text'),
+        title: Text(widget.destination.title),
         backgroundColor: widget.destination.color,
       ),
-      backgroundColor: widget.destination.color[100],
+      backgroundColor: widget.destination.color[50],
       body: Container(
         padding: const EdgeInsets.all(32.0),
         alignment: Alignment.center,
