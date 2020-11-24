65137bb7cce89d9ac99939dff5dab304ccf66c42
jackychan@virtuosity-global.com <jackychan@virtuosity-global.com>
2020-11-24 15:17:00 +0800

    add navigation bar scroll fade

--- lib/main.dart
+++ lib/main.dart
@@ -80,7 +80,7 @@ class ListPage extends StatelessWidget {
                   },
                   child: Center(
                     child: Text('Item $index',
-                        style: Theme.of(context).primaryTextTheme.display1),
+                        style: Theme.of(context).primaryTextTheme.headline4),
                   ),
                 ),
               ),
@@ -136,9 +136,11 @@ class _TextPageState extends State<TextPage> {
 }
 
 class DestinationView extends StatefulWidget {
-  const DestinationView({Key key, this.destination}) : super(key: key);
+  const DestinationView({Key key, this.destination, this.onNavigation})
+      : super(key: key);
 
   final Destination destination;
+  final VoidCallback onNavigation;
 
   @override
   _DestinationViewState createState() => _DestinationViewState();
@@ -148,6 +150,9 @@ class _DestinationViewState extends State<DestinationView> {
   @override
   Widget build(BuildContext context) {
     return Navigator(
+      observers: <NavigatorObserver>[
+        ViewNavigatorObserver(widget.onNavigation),
+      ],
       onGenerateRoute: (RouteSettings settings) {
         return MaterialPageRoute(
           settings: settings,
@@ -176,6 +181,7 @@ class _HomePageState extends State<HomePage>
     with TickerProviderStateMixin<HomePage> {
   List<Key> _destinationKeys;
   List<AnimationController> _faders;
+  AnimationController _hide;
   int _currentIndex = 0;
 
   @override
@@ -191,60 +197,107 @@ class _HomePageState extends State<HomePage>
     _destinationKeys =
         List<Key>.generate(allDestinations.length, (int index) => GlobalKey())
             .toList();
+    _hide = AnimationController(vsync: this, duration: kThemeAnimationDuration);
   }
 
   @override
   void dispose() {
     for (AnimationController controller in _faders) controller.dispose();
+    _hide.dispose();
     super.dispose();
+  }
+
+  bool _handleScrollNotification(ScrollNotification notification) {
+    if (notification.depth == 0) {
+      if (notification is UserScrollNotification) {
+        final UserScrollNotification userScroll = notification;
+        switch (userScroll.direction) {
+          case ScrollDirection.forward:
+            _hide.forward();
+            break;
+          case ScrollDirection.reverse:
+            _hide.reverse();
+            break;
+          case ScrollDirection.idle:
+            break;
+        }
+      }
+    }
+    return false;
   }
 
   @override
   Widget build(BuildContext context) {
-    return Scaffold(
-      body: SafeArea(
-        top: false,
-        child: Stack(
-          fit: StackFit.expand,
-          children: allDestinations.map((Destination destination) {
-            final Widget view = FadeTransition(
-              opacity: _faders[destination.index]
-                  .drive(CurveTween(curve: Curves.fastOutSlowIn)),
-              child: KeyedSubtree(
-                key: _destinationKeys[destination.index],
-                child: DestinationView(
-                  destination: destination,
+    return NotificationListener<ScrollNotification>(
+      onNotification: _handleScrollNotification,
+      child: Scaffold(
+        body: SafeArea(
+          top: false,
+          child: Stack(
+            fit: StackFit.expand,
+            children: allDestinations.map((Destination destination) {
+              final Widget view = FadeTransition(
+                opacity: _faders[destination.index]
+                    .drive(CurveTween(curve: Curves.fastOutSlowIn)),
+                child: KeyedSubtree(
+                  key: _destinationKeys[destination.index],
+                  child: DestinationView(
+                    destination: destination,
+                    onNavigation: () {
+                      _hide.forward();
+                    },
+                  ),
                 ),
-              ),
-            );
-            if (destination.index == _currentIndex) {
-              _faders[destination.index].forward();
-              return view;
-            } else {
-              _faders[destination.index].reverse();
-              if (_faders[destination.index].isAnimating) {
-                return IgnorePointer(child: view);
+              );
+              if (destination.index == _currentIndex) {
+                _faders[destination.index].forward();
+                return view;
+              } else {
+                _faders[destination.index].reverse();
+                if (_faders[destination.index].isAnimating) {
+                  return IgnorePointer(child: view);
+                }
+                return Offstage(child: view);
               }
-              return Offstage(child: view);
-            }
-          }).toList(),
+            }).toList(),
+          ),
+        ),
+        bottomNavigationBar: ClipRect(
+          child: SizeTransition(
+            sizeFactor: _hide,
+            axisAlignment: -1.0,
+            child: BottomNavigationBar(
+              currentIndex: _currentIndex,
+              onTap: (int index) {
+                setState(() {
+                  _currentIndex = index;
+                });
+              },
+              items: allDestinations.map((Destination destination) {
+                return BottomNavigationBarItem(
+                    icon: Icon(destination.icon),
+                    backgroundColor: destination.color,
+                    label: destination.title);
+              }).toList(),
+            ),
+          ),
         ),
       ),
-      bottomNavigationBar: BottomNavigationBar(
-        currentIndex: _currentIndex,
-        onTap: (int index) {
-          setState(() {
-            _currentIndex = index;
-          });
-        },
-        items: allDestinations.map((Destination destination) {
-          return BottomNavigationBarItem(
-              icon: Icon(destination.icon),
-              backgroundColor: destination.color,
-              title: Text(destination.title));
-        }).toList(),
-      ),
     );
+  }
+}
+
+class ViewNavigatorObserver extends NavigatorObserver {
+  ViewNavigatorObserver(this.onNavigation);
+
+  final VoidCallback onNavigation;
+
+  void didPop(Route<dynamic> route, Route<dynamic> previousRoute) {
+    onNavigation();
+  }
+
+  void didPush(Route<dynamic> route, Route<dynamic> previousRoute) {
+    onNavigation();
   }
 }
 
